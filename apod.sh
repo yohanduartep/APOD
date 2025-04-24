adbd0fe8d0e29f80a57c4304b6451231bea9dadc
yohanduartep <yohandpessanha@gmail.com>
2025-04-24 16:22:07 -0300

    Uploading script

---
+++ apod.sh
@@ -0,0 +1,5 @@
+DAILY_PATH="$HOME/Pictures/001.jpg"
+RANDOM_PATH="$HOME/Pictures/002.jpg"
+
+wget -q -O "$DAILY_PATH" "https://raw.githubusercontent.com/yohanduartep/APOD-Script/refs/heads/main/001.jpg"
+wget -q -O "$RANDOM_PATH" "https://raw.githubusercontent.com/yohanduartep/APOD-Script/refs/heads/main/002.jpg"
